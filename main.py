8122e22204050c1622ef873690f250d8a7e53a33
noWasabi111 <89679797+noWasabi111@users.noreply.github.com>
2024-05-27 22:38:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
-
-void main():
+def main() :
   directory = input("Enter the directory to list: ")
   command = f"ls {directory}"  # Vulnerable to Command Injection
   os.system(command)
-
 if __name__ == '__main__':
     main()  
